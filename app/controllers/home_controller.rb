84c17d5fbdcdae5653e8418372f121288cb581e6
Baldrani <mael.mayon@free.fr>
2018-06-13 13:53:01 +0200

    Add listing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      @websites = Website.where(user_id: current_user.id)
+      @websites = Website.where(user: current_user)
     else
       redirect_to "/users/sign_in"
     end
@@ -12,10 +12,16 @@ class HomeController < ApplicationController
     if not url =~ /(https:\/\/)?(www.)?.+\..+/
       flash[:warning] = "Votre url n'est pas valide !"
     else
+      flash[:success] = "Site bien ajouté !"
       @wbesite = Website.create(url: url, user: current_user)
     end
-
     redirect_to root_url
+  end
 
+  def removeWebsite
+    @website = Website.find(params[:id])
+    @website.destroy
+    flash[:success] = "Site bien retiré !"
+    redirect_to root_url
   end
 end
